d313253719e1e685a588f2751af1ef159afd582b
Patryk Jankowski <pjankowski@davinci-studio.eu>
2022-01-14 11:53:49 +0100

    Change style

--- src/app/search-dogs/dogs.component.ts
+++ src/app/search-dogs/dogs.component.ts
@@ -3,6 +3,9 @@ import { DogsService } from '../services/dogs.service';
 import { addFavorite } from '../store/store.actions';
 import { Store } from '@ngrx/store';
 import { v4 as uuid } from 'uuid';
+import { Observable, of, fromEvent } from 'rxjs';
+import { distinctUntilKeyChanged } from 'rxjs/operators';
+import { FavoriteDog } from '../models/favoriteDog.model';
 
 @Component({
   selector: 'dogs',
@@ -29,9 +32,41 @@ export class DogsComponent implements OnInit {
     });
   }
 
+  // favDog$: Observable<FavoriteDog> = {
+  //   breed: this.selectedBreed,
+  //   src: this.src,
+  //   id: uuid(),
+  // };
+
+  // observer = {
+  //   next: (): FavoriteDog => {return{
+  //     breed: this.selectedBreed,
+  //     src: this.src,
+  //     id: uuid(),
+  //   }}
+  // }
+
+  // addBtn$ = fromEvent(document, 'click');
+
+  // dog = this.addBtn$.subscribe(this.observer);
+
+  // favDog$ = new Observable<FavoriteDog>((subscriber) => {
+  //   subscriber.next(this.dog);
+  // });
+
+  // favoriteDog = this.favDog$
+  //   .pipe(distinctUntilKeyChanged('src'))
+  //   .subscribe((dog) => {
+  //     return (this.newDog = dog);
+  //   });
+
   handleAddClick() {
     this.store.dispatch(
-      addFavorite({ breed: this.selectedBreed, src: this.src, id: uuid() })
+      addFavorite({
+        breed: this.selectedBreed,
+        src: this.src,
+        id: uuid(),
+      })
     );
   }
 
